c0a67448cd75ae6d5c0ddcb1d7a065e73a3b5f9d
DanielAros <danielaros200@gmail.com>
2023-06-06 09:36:27 -0600

    funcionalidad de encriptar y desencriptar funcionando correctamente

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from random import sample
 alfabeto = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-texto_plano_num = []
+# valores = [3,17,22,9,23,18,24,20,4,6,16,10,19,5,25,14,2,21,1,26,8,15,0,11,13,12,7]
+texto_plano_num = [] #Arreglo para guardar los numeros que le corresponden
 num_clave = []
 cifrado_suma = []
 modulo = []
@@ -10,42 +12,40 @@ cifrado_resta = []
 texto_plano = 'HELLOWORLD'
 clave = 'FACULTADDEINFORMATICA'
 
-#Encriptar texto
-for i in range(len(texto_plano)):
-    texto_plano_num.append(alfabeto.index(texto_plano[i]))
-    num_clave.append(alfabeto.index(clave[i]))
+valores = sample(range(0,27), 27)
 
+def encriptar():
+    for i in range(len(texto_plano)):
+        indice = alfabeto.index(texto_plano[i])
+        texto_plano_num.append(valores[indice])
+        indice = alfabeto.index(clave[i])
+        num_clave.append(valores[indice])
 
-for i in range(len(texto_plano)):
-    cifrado_suma.append(texto_plano_num[i]+num_clave[i])
-    if cifrado_suma[i] >= 27:
-        modulo.append(cifrado_suma[i] % 27)
-        es_modulo.append(True)
-    else:
-        modulo.append(cifrado_suma[i])
-        es_modulo.append(False)
-    texto_cifrado.append(alfabeto[modulo[i]])
+    for i in range(len(texto_plano)):
+        cifrado_suma.append(texto_plano_num[i]+num_clave[i])
+        if cifrado_suma[i] >= 27:
+            modulo.append(cifrado_suma[i] % 27)
+            es_modulo.append(True)
+        else:
+            modulo.append(cifrado_suma[i])
+            es_modulo.append(False)
+        ind = valores.index(modulo[i])
+        texto_cifrado.append(alfabeto[ind])
+    print("".join(texto_cifrado))
 
-print("".join(texto_cifrado))
-print(texto_cifrado)
+def desencriptar():
+    palabra = []
+    for i in range(len(texto_cifrado)):
+        if(es_modulo[i]):
+            aux = modulo[i]
+            modulo[i] = 27 + aux
+        cifrado_resta.append(modulo[i] - num_clave[i])
+        ind = valores.index(cifrado_resta[i])
+        palabra.append(alfabeto[ind])
+    print("".join(palabra))
 
-
-palabra = []
-#Desencriptacion
-for i in range(len(texto_cifrado)):
-    if(es_modulo[i]):
-        aux = modulo[i]
-        modulo[i] = 27 + aux
-    cifrado_resta.append(modulo[i] - num_clave[i])
-    # index_letra = texto_plano_num.index(cifrado_resta[i])
-    palabra.append(alfabeto[cifrado_resta[i]])
-
-# print(texto_plano_num)
-print(cifrado_resta)
-print(palabra)
-
-
-
+encriptar()
+desencriptar()
 
 
 
